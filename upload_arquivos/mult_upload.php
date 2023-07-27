46528183e541a9b2eb0fef7cc0c9cf7919deefea
Daniel Silva <85084842+silvadaniell@users.noreply.github.com>
2023-07-27 15:57:37 -0300

    Add files via upload

---
+++ upload_arquivos/mult_upload.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplo_Upload</title>
+</head>
+<style>
+</style>
+<body>
+    <form action="mult_upload.php" method="POST" 
+    enctype="multipart/form-data">
+        <input type="file" name="arquivo[]" multiple/>
+        <button type="submit" name= "botao_enviar">Enviar</button>
+    </form>
+    
+</body>
+</html>
+
+<?php
+    if(isset($_POST['botao_enviar'])){
+        $tipos_permitidos = ['jpg', 'jpeg', 'gif', 'png', 'JPG',
+        'JPEG', 'GIF', 'PNG'];
+
+        $quant_arquivos = count($_FILES['arquivo']['name']);
+        $cont = 0;
+
+        while($cont < $quant_arquivos){
+            $extensao = pathinfo($_FILES['arquivo']['name']
+            [$cont],
+             PATHINFO_EXTENSION);
+            // VER SE O TIPO DE ARQUIVO EXISTE
+            if(in_array($extensao, $tipos_permitidos)){
+                $pasta = "arquivos/";
+                $temporario = $_FILES['arquivo']['tmp_name']
+                [$cont];
+                $novo_nome = uniqid().".$extensao";
+    
+                // fazer upload
+                if(move_uploaded_file($temporario, $pasta.
+                $novo_nome)){
+                    echo "<p>Upload realizado!</p>";
+                }else{
+                    echo "<p>Falha no upload!</p>";
+                }//fim upload
+            }else{
+                echo "<p> Tipo do arquivo não é permitido</p>";
+            }//fim do array
+            $cont++;
+        }//fim while
+
+    }//fim if isset
+
+
+?>
